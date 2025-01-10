4f68544e8ccb2d10017940a681eb46add10f4aa1
Jack <jackfisher.hsoi@gmail.com>
2025-01-10 04:50:23 -0800

    Created player movement (and normalized it)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,20 +5,21 @@ public class Player : MonoBehaviour
     [SerializeField] private float _speed = 1.0f;
     [SerializeField] private Transform _playerTransform;
     [SerializeField] private GameObject _plantPrefab;
-    [SerializeField] private int _numSeeds = 5; 
+    [SerializeField] private int _numSeeds = 5;
     [SerializeField] private PlantCountUI _plantCountUI;
 
+    [SerializeField] private Rigidbody2D _rigidBody2D;
     private int _numSeedsLeft;
     private int _numSeedsPlanted;
 
     private void Start ()
     {
-        
+       
     }
 
     private void Update()
     {
-        
+        _rigidBody2D.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized * _speed;
     }
 
     public void PlantSeed ()
