e1f47693a0f043e63528abb3eeff44db327629fc
Jack <jackfisher.hsoi@gmail.com>
2025-01-11 02:38:12 -0800

    Finished adding the UI functionality + built for itch

--- Assets/Scripts/PlantCountUI.cs
+++ Assets/Scripts/PlantCountUI.cs
@@ -5,9 +5,32 @@ public class PlantCountUI : MonoBehaviour
 {
     [SerializeField] private TMP_Text _plantedText;
     [SerializeField] private TMP_Text _remainingText;
+    [SerializeField] private Player player;
+    int seedsPlanted = 0;
 
+    private void Start()
+    {
+        _plantedText.text = seedsPlanted.ToString();
+        _remainingText.text = player._numSeeds.ToString();
+    }
+    void Update()
+    {
+        // Check if the space bar is pressed.
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // if there are still seeds
+            if (player._numSeeds > 0)
+            {
+                player._numSeeds--;
+                seedsPlanted++;
+                UpdateSeeds(player._numSeeds, seedsPlanted); // Update the UI
+            }
+        }
+    }
     public void UpdateSeeds (int seedsLeft, int seedsPlanted)
     {
-        
+        player.PlantSeed();
+        _plantedText.text = seedsPlanted.ToString();
+        _remainingText.text = player._numSeeds.ToString();
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,33 +5,23 @@ public class Player : MonoBehaviour
     [SerializeField] private float _speed = 1.0f;
     [SerializeField] private Transform _playerTransform;
     [SerializeField] private GameObject _plantPrefab;
-    [SerializeField] private int _numSeeds = 5;
+    [SerializeField] public int _numSeeds = 5;
     [SerializeField] private PlantCountUI _plantCountUI;
-
     [SerializeField] private Rigidbody2D _rigidBody2D;
-    private int _numSeedsLeft;
-    private int _numSeedsPlanted;
 
-    private void Start ()
-    {
-       
-    }
 
+  
     private void Update()
-    {
+    { 
         _rigidBody2D.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized * _speed;
-
-        if (Input.GetKeyDown(KeyCode.Space))
-
-        {
-
-            Instantiate(_plantPrefab, transform.position, Quaternion.identity);
-
-        }
     }
 
     public void PlantSeed ()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(_plantPrefab, transform.position, Quaternion.identity);
+        }
         
     }
 }
